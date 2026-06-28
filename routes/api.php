329eeb327770a527e57855498b7d35d9d238bd54
Aniq Abbas <aniqabbas5@gmail.com>
2026-06-28 21:08:51 +0800

    Protect admin API

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,44 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\DesignController;
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('designs', DesignController::class);
-Route::patch('/designs/{design}/featured', [DesignController::class, 'toggleFeatured']);
+$adminOnly = function (Request $request) {
+    $adminKey = env('ADMIN_API_KEY');
+
+    if (!$adminKey || $request->header('X-Admin-Key') !== $adminKey) {
+        abort(403, 'Unauthorized admin action.');
+    }
+};
+
+/*
+|--------------------------------------------------------------------------
+| Public routes
+|--------------------------------------------------------------------------
+*/
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+Route::get('/designs', [DesignController::class, 'index']);
+Route::get('/designs/{design}', [DesignController::class, 'show']);
+
+/*
+|--------------------------------------------------------------------------
+| Admin protected routes
+|--------------------------------------------------------------------------
+*/
+Route::middleware($adminOnly)->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::patch('/categories/{category}', [CategoryController::class, 'update']);
+    Route::put('/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+
+    Route::post('/designs', [DesignController::class, 'store']);
+    Route::patch('/designs/{design}', [DesignController::class, 'update']);
+    Route::put('/designs/{design}', [DesignController::class, 'update']);
+    Route::delete('/designs/{design}', [DesignController::class, 'destroy']);
+
+    Route::patch('/designs/{design}/featured', [DesignController::class, 'toggleFeatured']);
+});
